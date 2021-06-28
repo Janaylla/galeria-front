67c3cb31de77895a91c048156618837b11af5bba
Janaylla Firmino <janayllaf@gmail.com>
2021-06-28 16:45:11 -0300

    Instalações basicas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Router from "../src/router/Router";
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Router/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/router/Router.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+const Router = () => {
+    return (
+      <BrowserRouter>
+        <Switch>
+        {/* <Route exact path="/" component={Feed}/> */}
+        </Switch>
+      </BrowserRouter>
+    );
+  };
+  
+  export default Router;
